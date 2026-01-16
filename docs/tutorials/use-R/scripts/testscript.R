8253a929c37f0611217d349323b7d0bd898489cc
Martin Lind <martin.i.lind@gmail.com>
2026-01-16 10:13:38 +0100

    Adding a script file to teach how scripts work

---
+++ docs/tutorials/use-R/scripts/testscript.R
@@ -0,0 +1,24 @@
+# Det här är ett testscript där vi tittar på hur ett script fungerar
+
+# Först lite matematik, R är en väldigt bra miniräknare
+
+1+1
+
+20/53
+
+sqrt(8)
+
+pi
+
+# Hur mycket höjdes studiemedlet mellan 2024 och 2025?
+
+# Skapa objekt med studiemedel för respektive år
+studiemedel2024 <- 13156
+studiemedel2025 <-13500
+
+# Beräkna höjningen i kronor
+studiemedel2025 - studiemedel2024
+
+# Beräkna den procentuelle höjningen
+(studiemedel2025 - studiemedel2024) / studiemedel2024 * 100
+
